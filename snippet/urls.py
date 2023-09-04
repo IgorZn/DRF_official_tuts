d3ed05d646916f9ebba301f736df019a63b51df8
IgorZn <igor.znamensky@gmail.com>
2023-09-04 10:26:31 +0300

    next: tutorial part 3

--- snippet/urls.py
+++ snippet/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from .views import snippet_list, snippet_detail
 
 urlpatterns = [
     path('', snippet_list),
     path('<int:pk>', snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
